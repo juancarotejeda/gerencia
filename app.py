bdda11af392a8fe1dc580e384c6c160988affebb
juan caro <juancarotejeda@gmail.com>
2024-11-22 09:53:41 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,280 @@
+import mysql.connector,funciones,os
+from mysql.connector import errorcode
+from flask import Flask, render_template,flash, request,  redirect, url_for
+from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key=os.getenv("APP_KEY")
+DB_HOST =os.getenv('DB_HOST')
+DB_USERNAME =os.getenv("DB_USERNAME")
+DB_PASSWORD =os.getenv("DB_PASSWORD")
+DB_NAME =os.getenv("DB_NAME")
+
+
+connection =mysql.connector.connect(
+    host=DB_HOST,
+    user=DB_USERNAME,
+    password=DB_PASSWORD,
+    database=DB_NAME,
+    autocommit=True
+)
+
+@app.route("/")
+def login():   
+    cur = connection.cursor() 
+    resultado=funciones.listado_administrativo(cur,"administracion")
+    funcion=[]
+    for funcionarios in resultado:
+       funcion += funcionarios  
+    cur.close()                   
+    return render_template('login.html',funcion=funcion)
+
+@app.route("/verificacion", methods=["GUET","POST"])
+def verificacion(): 
+   msg = ''
+   if request.method == 'POST':        
+    funcion = request.form['funcion']
+    codigo_personal = request.form['codigo_personal']
+    password = request.form['clave']   
+    cur = connection.cursor()   
+    estacion=funciones.check_funcion(cur,funcion)
+   if estacion != False:           
+    if codigo_personal == estacion[0] and password == estacion[1] : 
+      if codigo_personal == 'GE-01': 
+            paradas=funciones.listado_paradas(cur)
+            parada=[]       
+            fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+            informacion=funciones.info_parada(cur,parada)
+            cabecera=funciones.info_cabecera(cur,parada) 
+            miembros=funciones.lista_miembros(cur,parada)                
+            diario=funciones.diario_general(cur,parada) 
+            cuotas_hist=funciones.prestamo_aport(cur,parada)
+            cur.close()
+            return render_template('index.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)                  
+        
+      elif codigo_personal == 'GRN-00':
+            cur = connection.cursor() 
+            paradas=funciones.listado_paradas_norte(cur)
+            parada=[]  
+            fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+            informacion=funciones.info_parada(cur,parada)
+            cabecera=funciones.info_cabecera(cur,parada) 
+            miembros=funciones.lista_miembros(cur,parada)                
+            diario=funciones.diario_general(cur,parada) 
+            cuotas_hist=funciones.prestamo_aport(cur,parada)
+            cur.close()
+            return render_template('index_norte.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)      
+
+      elif codigo_personal == 'GRS-00':
+            cur = connection.cursor() 
+            paradas=funciones.listado_paradas_sur(cur)
+            parada=[]  
+            fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+            informacion=funciones.info_parada(cur,parada)
+            cabecera=funciones.info_cabecera(cur,parada) 
+            miembros=funciones.lista_miembros(cur,parada)                
+            diario=funciones.diario_general(cur,parada) 
+            cuotas_hist=funciones.prestamo_aport(cur,parada)
+            cur.close()
+            return render_template('index_sur.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)      
+
+      elif codigo_personal == 'GRE-00':
+            cur = connection.cursor() 
+            paradas=funciones.listado_paradas_este(cur)
+            parada=[]  
+            fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+            informacion=funciones.info_parada(cur,parada)
+            cabecera=funciones.info_cabecera(cur,parada) 
+            miembros=funciones.lista_miembros(cur,parada)                
+            diario=funciones.diario_general(cur,parada) 
+            cuotas_hist=funciones.prestamo_aport(cur,parada)
+            cur.close()
+            return render_template('index_este.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)      
+ 
+      elif codigo_personal == 'GDN-00':
+            cur = connection.cursor() 
+            paradas=funciones.listado_paradas_distrito(cur)
+            parada=[]  
+            fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+            informacion=funciones.info_parada(cur,parada)
+            cabecera=funciones.info_cabecera(cur,parada) 
+            miembros=funciones.lista_miembros(cur,parada)                
+            diario=funciones.diario_general(cur,parada) 
+            cuotas_hist=funciones.prestamo_aport(cur,parada)
+            cur.close()
+            return render_template('index_distrito.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)      
+  
+      elif codigo_personal == 'GSN-00':
+            cur = connection.cursor() 
+            paradas=funciones.listado_paradas_sdn(cur)
+            parada=[]  
+            fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+            informacion=funciones.info_parada(cur,parada)
+            cabecera=funciones.info_cabecera(cur,parada) 
+            miembros=funciones.lista_miembros(cur,parada)                
+            diario=funciones.diario_general(cur,parada) 
+            cuotas_hist=funciones.prestamo_aport(cur,parada)
+            cur.close()
+            return render_template('index_sdn.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)      
+   
+      elif codigo_personal == 'GSE-00':
+            cur = connection.cursor() 
+            paradas=funciones.listado_paradas_sde(cur)
+            parada=[]  
+            fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+            informacion=funciones.info_parada(cur,parada)
+            cabecera=funciones.info_cabecera(cur,parada) 
+            miembros=funciones.lista_miembros(cur,parada)                
+            diario=funciones.diario_general(cur,parada) 
+            cuotas_hist=funciones.prestamo_aport(cur,parada)
+            cur.close()
+            return render_template('index_sde.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)      
+    
+      elif codigo_personal == 'GSO-00':
+            cur = connection.cursor() 
+            paradas=funciones.listado_paradas_sdo(cur)
+            parada=[]  
+            fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+            informacion=funciones.info_parada(cur,parada)
+            cabecera=funciones.info_cabecera(cur,parada) 
+            miembros=funciones.lista_miembros(cur,parada)                
+            diario=funciones.diario_general(cur,parada) 
+            cuotas_hist=funciones.prestamo_aport(cur,parada)
+            cur.close()
+            return render_template('index_sdo.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)      
+     
+    else:
+        msg = 'Sin acceso a estas paginas!'          
+        flash(msg)           
+        return redirect(url_for('login')) 
+       
+@app.route("/indice", methods=["GUET","POST"])
+def indice():
+    if request.method == 'POST':        
+        parada = request.form['parada'] 
+        cur = connection.cursor()          
+        fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+        informacion=funciones.info_parada(cur,parada) 
+        cabecera=funciones.info_cabecera(cur,parada) 
+        miembros=funciones.lista_miembros(cur,parada)                
+        diario=funciones.diario_general(cur,parada) 
+        cuotas_hist=funciones.prestamo_aport(cur,parada)
+        paradas=funciones.listado_paradas_norte(cur)
+        cur.close()
+        return render_template('index.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)     
+
+@app.route("/indice_note", methods=["GUET","POST"])
+def indice_note():
+    if request.method == 'POST':        
+        parada = request.form['parada'] 
+        cur = connection.cursor()          
+        fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+        informacion=funciones.info_parada(cur,parada) 
+        cabecera=funciones.info_cabecera(cur,parada) 
+        miembros=funciones.lista_miembros(cur,parada)                
+        diario=funciones.diario_general(cur,parada) 
+        cuotas_hist=funciones.prestamo_aport(cur,parada)
+        paradas=funciones.listado_paradas_norte(cur)
+        cur.close()
+        return render_template('index_norte.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)     
+
+@app.route("/indice_sur", methods=["GUET","POST"])
+def indice_sur():
+    if request.method == 'POST':        
+        parada = request.form['parada'] 
+        cur = connection.cursor()          
+        fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+        informacion=funciones.info_parada(cur,parada) 
+        cabecera=funciones.info_cabecera(cur,parada) 
+        miembros=funciones.lista_miembros(cur,parada)                
+        diario=funciones.diario_general(cur,parada) 
+        cuotas_hist=funciones.prestamo_aport(cur,parada)
+        paradas=funciones.listado_paradas_sur(cur)
+        cur.close()
+        return render_template('index_sur.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)     
+
+@app.route("/indice_este", methods=["GUET","POST"])
+def indice_este():
+    if request.method == 'POST':        
+        parada = request.form['parada'] 
+        cur = connection.cursor()          
+        fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+        informacion=funciones.info_parada(cur,parada) 
+        cabecera=funciones.info_cabecera(cur,parada) 
+        miembros=funciones.lista_miembros(cur,parada)                
+        diario=funciones.diario_general(cur,parada) 
+        cuotas_hist=funciones.prestamo_aport(cur,parada)
+        paradas=funciones.listado_paradas_este(cur)
+        cur.close()
+        return render_template('index_este.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)     
+
+@app.route("/indice_sdn", methods=["GUET","POST"])
+def indice_sdn():
+    if request.method == 'POST':        
+        parada = request.form['parada'] 
+        cur = connection.cursor()          
+        fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+        informacion=funciones.info_parada(cur,parada) 
+        cabecera=funciones.info_cabecera(cur,parada) 
+        miembros=funciones.lista_miembros(cur,parada)                
+        diario=funciones.diario_general(cur,parada) 
+        cuotas_hist=funciones.prestamo_aport(cur,parada)
+        paradas=funciones.listado_paradas_sdn(cur)
+        cur.close()
+        return render_template('index_sdn.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)     
+
+@app.route("/indice_sde", methods=["GUET","POST"])
+def indice_sde():
+    if request.method == 'POST':        
+        parada = request.form['parada'] 
+        cur = connection.cursor()          
+        fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+        informacion=funciones.info_parada(cur,parada) 
+        cabecera=funciones.info_cabecera(cur,parada) 
+        miembros=funciones.lista_miembros(cur,parada)                
+        diario=funciones.diario_general(cur,parada) 
+        cuotas_hist=funciones.prestamo_aport(cur,parada)
+        paradas=funciones.listado_paradas_sde(cur)
+        cur.close()
+        return render_template('index_sde.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)     
+
+@app.route("/indice_sdo", methods=["GUET","POST"])
+def indice_sdo():
+    if request.method == 'POST':        
+        parada = request.form['parada'] 
+        cur = connection.cursor()          
+        fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+        informacion=funciones.info_parada(cur,parada) 
+        cabecera=funciones.info_cabecera(cur,parada) 
+        miembros=funciones.lista_miembros(cur,parada)                
+        diario=funciones.diario_general(cur,parada) 
+        cuotas_hist=funciones.prestamo_aport(cur,parada)
+        paradas=funciones.listado_paradas_sdo(cur)
+        cur.close()
+        return render_template('index_sdo.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)     
+
+@app.route("/indice_distrito", methods=["GUET","POST"])
+def indice_distrito():
+    if request.method == 'POST':        
+        parada = request.form['parada'] 
+        cur = connection.cursor()          
+        fecha = datetime.strftime(datetime.now(),"%Y %m %d - %H")
+        informacion=funciones.info_parada(cur,parada) 
+        cabecera=funciones.info_cabecera(cur,parada) 
+        miembros=funciones.lista_miembros(cur,parada)                
+        diario=funciones.diario_general(cur,parada) 
+        cuotas_hist=funciones.prestamo_aport(cur,parada)
+        paradas=funciones.listado_paradas_distrito(cur)
+        cur.close()
+        return render_template('index_distrito.html',paradas=paradas,informacion=informacion,cabecera=cabecera,fecha=fecha,miembros=miembros,diario=diario,cuotas_hist=cuotas_hist)     
+
+@app.route("/canal")
+def canal():
+    return render_template('canal_motoben.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0')
---
+++ funciones.py
@@ -0,0 +1,234 @@
+def check_parada(cur,parada):
+    cur.execute(f"SELECT balance_banco FROM tabla_index WHERE nombre = '{parada}' ")
+    check=cur.fetchall()
+    for valor in check:
+        if valor[0] > 1000.00:
+            return True
+        else: 
+            return False
+        
+def check_funcion(cur,funcion):
+    cur.execute(f"SELECT codigo,password,funcion FROM administracion WHERE funcion = '{funcion}' ")
+    check=cur.fetchall()
+    if check !=[]:
+      for valor in check:
+        codg=valor[0]
+        pasw=valor[1]
+        func=valor[2]
+      return codg,pasw,func
+    else: 
+        return False        
+        
+        
+        
+        
+    
+def listado_paradas(cur):
+    nombre=[]
+    cur.execute("SELECT nombre FROM tabla_index")  
+    db_paradas=cur.fetchall()
+    for nomb in db_paradas:
+        nombre+=nomb     
+    return nombre
+
+def listado_paradas_norte(cur):
+    nombre=[]
+    cur.execute("SELECT nombre FROM tabla_index WHERE zona='Norte'")  
+    db_paradas=cur.fetchall()
+    for nomb in db_paradas:
+        nombre+=nomb     
+    return nombre
+
+
+def listado_paradas_sur(cur):
+    nombre=[]
+    cur.execute("SELECT nombre FROM tabla_index WHERE zona='Sur'")  
+    db_paradas=cur.fetchall()
+    for nomb in db_paradas:
+        nombre+=nomb     
+    return nombre
+
+def listado_paradas_este(cur):
+    nombre=[]
+    cur.execute("SELECT nombre FROM tabla_index WHERE zona='Este'")  
+    db_paradas=cur.fetchall()
+    for nomb in db_paradas:
+        nombre+=nomb     
+    return nombre
+
+def listado_paradas_distrito(cur):
+    nombre=[]
+    cur.execute("SELECT nombre FROM tabla_index WHERE zona='Distrito Nacional'")  
+    db_paradas=cur.fetchall()
+    for nomb in db_paradas:
+        nombre+=nomb     
+    return nombre
+
+def listado_paradas_sdn(cur):
+    nombre=[]
+    cur.execute("SELECT nombre FROM tabla_index WHERE zona='Sdn'")  
+    db_paradas=cur.fetchall()
+    for nomb in db_paradas:
+        nombre+=nomb     
+    return nombre
+
+def listado_paradas_sde(cur):
+    nombre=[]
+    cur.execute("SELECT nombre FROM tabla_index WHERE zona='Sde'")  
+    db_paradas=cur.fetchall()
+    for nomb in db_paradas:
+        nombre+=nomb     
+    return nombre
+
+def listado_paradas_sdo(cur):
+    nombre=[]
+    cur.execute("SELECT nombre FROM tabla_index WHERE zona='Sdo'")  
+    db_paradas=cur.fetchall()
+    for nomb in db_paradas:
+        nombre+=nomb     
+    return nombre
+
+
+
+def listado_administrativo(cur,adm):
+    cur.execute(f"SELECT funcion FROM {adm}")  
+    db_paradas=cur.fetchall()     
+    return db_paradas
+
+def info_parada(cur,parada):
+    if parada !=[]:
+      cur.execute(f"SELECT codigo,nombre,direccion,municipio,provincia,zona,cuota,pago,banco,num_cuenta FROM  tabla_index  WHERE nombre='{parada}'" )
+      infos=cur.fetchall()     
+      return infos
+    else:
+      return [] 
+
+def info_cabecera(cur,parada):
+    if parada !=[]:          
+        cur.execute(f'SELECT nombre FROM {parada}')
+        seleccion=cur.fetchall()
+        cant=len(seleccion)  
+        presidente = []       
+        cur.execute(f"SELECT nombre FROM {parada}  WHERE funcion = 'Presidente'")   
+        press=cur.fetchone()
+        if press != None:  
+            for pres in press:   
+                presidente=pres
+        else:     
+            presidente='No disponible'   
+        veedor = []
+        cur.execute(f"SELECT nombre FROM {parada}  WHERE funcion = 'Veedor'")   
+        presd=cur.fetchone()
+        if presd != None:
+          for prex in presd:
+            veedor=prex 
+        else:
+            veedor='No disponible'           
+        return cant,presidente,veedor 
+    else:
+        return 0,'No disponible','No disponible'
+               
+     
+def lista_miembros(cur,parada):
+    if parada !=[]: 
+      listas=[]
+      cur.execute(f"SELECT id,nombre,cedula,telefono,funcion  FROM {parada}")
+      miembros=cur.fetchall()
+      for miembro in miembros:     
+         listas+=miembro    
+      lista=dividir_lista(listas,5)    
+      return lista
+    else:
+      return []  
+    
+def diario_general(cur,parada):
+    if parada !=[]: 
+        prestamos=[]
+        ingresos=[]
+        gastos=[]
+        aporte=[]
+        pendiente=[]
+        abonos=[]
+        balance_bancario=[]
+        cur.execute(f"SELECT  prestamos, ingresos, gastos, aporte, pendiente, abonos, balance_banco FROM tabla_index WHERE nombre='{parada}' " )  
+        consult=cur.fetchall()
+        for valor in consult:
+            prestamos=valor[0]
+            ingresos=valor[1]
+            gastos=valor[2]
+            aporte=valor[3]
+            pendiente=valor[4]
+            abonos=valor[5]
+            balance_bancario=valor[6]
+        balance=(aporte + ingresos + abonos )-(gastos+prestamos)
+        data=(balance,prestamos,ingresos,gastos,aporte,pendiente,abonos,balance_bancario)   
+        return data
+    else:
+        return 0,0,0,0,0,0,0,0
+
+def prestamo_aport(cur,parada):
+    cur.execute(f"SHOW TABLES LIKE '{parada}_cuota'")
+    vericar=cur.fetchall()
+    if vericar !=[]:
+      vgral=[]
+      cur.execute(f"SELECT nombre FROM {parada}")
+      list_nomb=cur.fetchall()
+      for nombre in list_nomb:
+         cur.execute(f"SELECT COUNT(estado) FROM {parada}_cuota WHERE estado = 'pago' and nombre='{nombre[0]}'") 
+         var_x = cur.fetchall()
+         for var_p in var_x:
+              var1=var_p[0]
+         cur.execute(f"SELECT COUNT(estado) FROM {parada}_cuota WHERE estado = 'no_pago' and nombre='{nombre[0]}'")
+         var_z = cur.fetchall()
+         for var_n in var_z:
+              var2=var_n[0]   
+         sub_t=var1+var2
+         if sub_t != 0 :    
+          avg=round((var1/sub_t)*100,2)
+         else:
+          avg = 0.00               
+         vgral+=(nombre[0],var1,var2,sub_t,avg) 
+      list_1=dividir_lista(vgral,5)                    
+      return list_1
+    else:
+      return [] 
+
+
+
+
+def dividir_lista(lista,lon) : 
+    return [lista[n:n+lon] for n in range(0,len(lista),lon)]     
+
+
+def aportacion(cur,parada):           
+    cur.execute(f"SELECT codigo, nombre, cedula, telefono, funcion FROM {parada}")
+    data=cur.fetchall()
+    return data
+  
+def verif_p(cur,parada,cedula,password):
+    cur.execute(f"SELECT * FROM tabla_index WHERE  adm_password = '{password}'")
+    result=cur.fetchall()
+    if result:
+      cur.execute(f"SELECT * FROM {parada} WHERE  cedula = '{cedula}'")                                       
+      accounts =cur.fetchall()
+      if accounts != []:
+         return True
+      else:
+         return False 
+    else: 
+        return False
+
+def nombres_miembro(cur,parada):
+        listado=[]
+        cur.execute(f"SELECT nombre FROM {parada} ")
+        nombres=cur.fetchall()
+        for nombre in nombres:
+            listado += nombre
+        return listado 
+
+
+def dat_miembros(cur,parada,miembro):
+    cur.execute(f"SELECT nombre,cedula,telefono,funcion FROM {parada} WHERE nombre='{miembro}'")
+    listado=cur.fetchall()
+    return listado
+
